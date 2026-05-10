bc3a1e5986ffa48a08a768206ee3f13ad76aefe6
MNoor-18 <alhwsawynwr@gmail.com>
2026-05-10 07:41:39 +0300

    some changes

--- components/FirstShow.tsx
+++ components/FirstShow.tsx
@@ -4,7 +4,7 @@ const FirstShow = () => {
   return (
     <section className="relative h-svh w-full overflow-hidden flex flex-col justify-end bg-linear-to-b from-primary to-white">
         <div className="absolute inset-0 z-50 w-full h-full flex justify-center items-center">
-            <h1 className="h-28 py-4 text-8xl font-bold bg-linear-to-l from-primary to-[#4F82DB] bg-clip-text text-transparent">
+            <h1 className="min-h-fit pb-10 text-8xl font-bold bg-linear-to-l from-primary to-[#4F82DB] bg-clip-text text-transparent">
               طبيبي
             </h1>
         </div>
